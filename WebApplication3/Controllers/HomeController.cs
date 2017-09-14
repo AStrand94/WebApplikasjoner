0c0657321897bedb01007b2d08ddfba1d87f2341
Stian Tornholm Grimsgaard <s305537@stud.hioa.no>
2017-09-14 16:08:03 +0200

    Added registration.cshtml

--- WebApplication3/Controllers/HomeController.cs
+++ WebApplication3/Controllers/HomeController.cs
@@ -15,5 +15,28 @@ namespace WebApplication3.Controllers
             var instance = db.Customers.Find(1);
             return View(instance);
         }
+
+        public ActionResult Registration()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Registration(Models.Customer customerIn)
+        {
+
+            try
+            {
+                db.Customers.Add(customerIn);
+                db.SaveChanges();
+            }
+            catch (Exception e)
+            {
+
+            }
+
+            return RedirectToAction("Index");
+        } 
+     
     }
 }

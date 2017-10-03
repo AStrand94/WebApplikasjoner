0af032e1b66628d8b0fb81ffc9796b84d9260b43
Andreas Strand <s305036@stud.hioa.no>
2017-10-03 17:56:19 +0200

    Fixed validation on Payment page

--- WebApplication3/Scripts/Javascript/Payment.js
+++ WebApplication3/Scripts/Javascript/Payment.js
@@ -12,26 +12,25 @@
             lastInput = input;
         }
     });
-
-
-    var onSubmitForm = function () {
-        cardNumber = $("#cardNumber");
-
-        if (!cardNumber.val()) {
-            alert("Card Number is required");
-            return false;
-        }
-        if (!$.isNumeric(cardNumber.val())) {
-            alert("Card Number can only have numbers");
-            return false;
-        }
-        if (!$('#expDate').val()) {
-            alert("Expiration Date is required.");
-            return false;
-        }
-        if (!$('#cvc').val()) {
-            alert("CVC is required.");
-            return false;
-        }
-    }
 });
+
+var onSubmitForm = function () {
+
+    if ($('#cardNumber').val().length != 16) {
+        alert("Cardnumber must be 16 digit!");
+        return false;
+    }
+    if (!$.isNumeric($("#cardNumber").val())) {
+        alert("Card Number can only have numbers");
+        return false;
+    }
+    if (!$('#expDate').val()) {
+        alert("Expiration Date is required.");
+        return false;
+    }
+    if ($('#cvc').val().length != 3) {
+        alert("CVC must be 3 digit!");
+        return false;
+    }
+    return true;
+}

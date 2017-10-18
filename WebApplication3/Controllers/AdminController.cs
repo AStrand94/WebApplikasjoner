ace45e3d9b9b095d61422cabedf7efc904302b5d
Stian Tornholm Grimsgaard <s305537@stud.hioa.no>
2017-10-18 15:47:47 +0200

    Added Log out, removed footer, and loggedIn check

--- WebApplication3/Controllers/AdminController.cs
+++ WebApplication3/Controllers/AdminController.cs
@@ -9,48 +9,92 @@ namespace WebApplication3.Controllers
 {
     public class AdminController : Controller
     {
-        // GET: Admin
         public ActionResult Login(string username, string password)
         {
             LoginBLL loginBLL = new LoginBLL();
 
             if(loginBLL.checkLogin(username, password))
             {
+                Session["loggedIn"] = true;
                 return RedirectToAction("Index");
             }
 
             return RedirectToAction("Index", "Home", new { area = "" });
-
+        }
+        
+        public ActionResult LogOut()
+        {
+            Session["loggedIn"] = false;
+            return RedirectToAction("Index", "Home", new { area = "" });
         }
 
         public ActionResult Index()
         {
+            if(!UserIsLoggedIn())
+            {
+                return RedirectToAction("Index", "Home", new { area = "" });
+            }
+
             return View();
         }
 
         public ActionResult Airplanes()
         {
+            if (!UserIsLoggedIn())
+            {
+                return RedirectToAction("Index", "Home", new { area = "" });
+            }
+
             return View(new AirplaneBLL().GetAllAirplanes());
         }
 
         public ActionResult Airports()
         {
+            if (!UserIsLoggedIn())
+            {
+                return RedirectToAction("Index", "Home", new { area = "" });
+            }
+
             return View(new AirportBLL().getAllAirports());
         }
 
         public ActionResult Customers()
         {
+            if (!UserIsLoggedIn())
+            {
+                return RedirectToAction("Index", "Home", new { area = "" });
+            }
+
             return View(new CustomerBLL().GetAllCustomers());
         }
 
         public ActionResult Flights()
         {
+            if (!UserIsLoggedIn())
+            {
+                return RedirectToAction("Index", "Home", new { area = "" });
+            }
+
             return View(new FlightBLL().GetAllFlights());
         }
 
         public ActionResult Routes()
         {
+            if (!UserIsLoggedIn())
+            {
+                return RedirectToAction("Index", "Home", new { area = "" });
+            }
+
             return View(new RouteBLL().GetAllRoutes());
+        }
+
+        public bool UserIsLoggedIn()
+        {
+            if (Session["loggedIn"] == null || Session["loggedIn"].Equals(false))
+            {
+                return false;
+            }
+            return true;
         }
 
     }

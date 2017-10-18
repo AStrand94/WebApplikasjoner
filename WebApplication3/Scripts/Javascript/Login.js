4d54b8eda9b5c3c4a0e045e413a6cf0225f5c6d2
Stian Tornholm Grimsgaard <s305537@stud.hioa.no>
2017-10-18 13:38:32 +0200

    Login modale added

---
+++ WebApplication3/Scripts/Javascript/Login.js
@@ -0,0 +1,25 @@
+﻿$(document).ready(function () {
+
+    $('#loginButton').on('click', function () {
+        var str = $('#loginButton').val();
+
+        
+    });
+
+    $('#username').keypress(function (e) {
+        if (e.which == 13) {
+            
+        }
+    });
+
+    $('#password').keypress(function (e) {
+        if (e.which == 13) {
+
+        }
+    });
+
+    $('.closeModal').on('click', function () {
+        $('#username').val('');
+        $('#password').val('');
+    });
+});

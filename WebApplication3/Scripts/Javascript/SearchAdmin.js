31c2bd81cfbcda618c6c0ca6faa645a4f4c47a08
Stian Tornholm Grimsgaard <s305537@stud.hioa.no>
2017-10-20 12:41:02 +0200

    Added dynamic Search

---
+++ WebApplication3/Scripts/Javascript/SearchAdmin.js
@@ -0,0 +1,17 @@
+﻿$(document).ready(function () {
+
+$(".search").on('input', function () {
+    var input = $(".search").val().toLowerCase();
+
+    $(".searchItem").each(function () {
+        var item = $(this).text().toLowerCase();
+
+        if (!item.includes(input)) {
+            $(this).parent().parent().parent().hide();
+        } else {
+            $(this).parent().parent().parent().show();
+        }
+    });
+});
+
+});

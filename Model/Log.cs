f512628c0970dfbc74a11608ab33f3779feafd5e
Andreas Strand <s305036@stud.hioa.no>
2017-10-27 10:00:27 +0200

    Changes to database is now being logged in a new table

---
+++ Model/Log.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApplication3.Model
+{
+    public class Log
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Entity { get; set; }
+
+        [Required]
+        [StringLength(14)]
+        public string Type { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+    }
+}
